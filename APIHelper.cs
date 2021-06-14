8e59f9ce70f90d019f2f7367ff2d02ad95ba83d6
kheslinga <84016572+kheslinga@users.noreply.github.com>
2021-06-14 18:35:22 +0200

    Add files via upload

---
+++ APIHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace AnagramSolver
+{
+     public static class APIHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public static async void test()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/resources/dictionary/lookup?source=English&target=English&input=Home"),
+                Headers =
+    {
+        { "x-rapidapi-key", "2d14de18e8msh9afedd327c23c5ap14da46jsn27f5e30bb14c" },
+        { "x-rapidapi-host", "systran-systran-platform-for-language-processing-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net.Http;
+
+
+namespace AnagramSolver
+{
+    class Program
+    {
+        static List<String> permutations = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Specify language: [NL] or [ENG]");
+            String language = Console.ReadLine();
+            Console.WriteLine("Enter string to check for anagrams");
+            char[] input = Console.ReadLine().ToCharArray();
+            DoPermutation(input, 0);
+            foreach(String str in permutations)
+            {
+                Console.WriteLine(str);
+            }
+            APIHelper.test();
+            Console.ReadLine();
+        }
+        static void DoPermutation(char[] charArray, int index)
+        {
+            if(index == charArray.Length - 1)
+            {
+                permutations.Add(new string(charArray));
+                return;
+            }
+            for(int i = index; i < charArray.Length; i++)
+            {
+                DoPermutation(SwapPositions(charArray, index, i),index + 1);
+            }
+        }
+        static char[] SwapPositions(char[] charArray, int a, int b)
+        {
+            char temp = charArray[b];
+            charArray[b] = charArray[a];
+            charArray[a] = temp;
+            return charArray;
+        }
+    }
+}
